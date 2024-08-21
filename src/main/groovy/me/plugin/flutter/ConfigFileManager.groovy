ee3679fe8c3c812a9a241a049fb6844fe9628be3
liutie0201 <liuxiangping0201@gmail.com>
2024-08-21 12:01:14 +0800

    暂不支持Linux属性

--- src/main/groovy/me/plugin/flutter/ConfigFileManager.groovy
+++ src/main/groovy/me/plugin/flutter/ConfigFileManager.groovy
@@ -85,8 +85,9 @@ class ConfigFileManager {
         configFile.withWriter("UTF-8") { writer ->
             // 添加全局注释
             writer.println("## 以下字段是自动生成的，删除或者为空都不影响应用的真实值，但是如果一旦赋值，将会以下面字段的值为准")
-            writer.println("## config.properties不仅仅只要有这些字段，还有以平台开头的值，比如applicationIdAndroid、applicationIdIOS、applicationIdWeb、applicationIdWindows、applicationIdMacOs、applicationNameIOS、applicationVersionCodeWeb、applicationVersionNameWindows、等等")
-            writer.println("## 如果将applicationVersionCode(仅举例) 赋值：applicationVersionCode = 101 ，而applicationVersionCodeAndroid = 105，那么其他平台(IOS、Web、Windows、MacOs、Linux)的applicationVersionCode都是101，而Android的applicationVersionCode则是105\n")
+            writer.println("## config.properties不仅仅只要有这些字段，还有以平台结尾的值，比如applicationIdAndroid、applicationIdIOS、applicationIdWeb、applicationIdWindows、applicationIdMacOs、applicationNameIOS、applicationVersionCodeWeb、applicationVersionNameWindows、等等")
+            writer.println("## 如果将applicationVersionCode(仅举例) 赋值：applicationVersionCode = 101 ，而applicationVersionCodeAndroid = 105，那么其他平台(IOS、Web、Windows、MacOs、)的applicationVersionCode都是101，而Android的applicationVersionCode则是105\n")
+            writer.println("## 暂不支持Linux平台\n")
 
             // 全局属性
             writer.println("## 全局属性")
@@ -96,47 +97,47 @@ class ConfigFileManager {
             writer.println("applicationVersionName=")
             writer.println("#loadAssetsName=")
 
-            // Android平台属性
-            writer.println("\n## Android")
-            writer.println("#applicationIdAndroid=")
-            writer.println("#applicationNameAndroid=")
-            writer.println("#applicationVersionCodeAndroid=")
-            writer.println("#applicationVersionNameAndroid=")
-
-            // iOS平台属性
-            writer.println("\n## IOS")
-            writer.println("#applicationIdIOS=")
-            writer.println("#applicationNameIOS=")
-            writer.println("#applicationVersionCodeIOS=")
-            writer.println("#applicationVersionNameIOS=")
-
-            // Web平台属性
-            writer.println("\n## Web")
-            writer.println("#applicationIdWeb=")
-            writer.println("#applicationNameWeb=")
-            writer.println("#applicationVersionCodeWeb=")
-            writer.println("#applicationVersionNameWeb=")
-
-            // Windows平台属性
-            writer.println("\n## Windows")
-            writer.println("#applicationIdWindows=")
-            writer.println("#applicationNameWindows=")
-            writer.println("#applicationVersionCodeWindows=")
-            writer.println("#applicationVersionNameWindows=")
-
-            // MacOS平台属性
-            writer.println("\n## MacOS")
-            writer.println("#applicationIdMacOs=")
-            writer.println("#applicationNameMacOs=")
-            writer.println("#applicationVersionCodeMacOs=")
-            writer.println("#applicationVersionNameMacOs=")
-
-            // Linux平台属性
-            writer.println("\n## Linux")
-            writer.println("#applicationIdLinux=")
-            writer.println("#applicationNameLinux=")
-            writer.println("#applicationVersionCodeLinux=")
-            writer.println("#applicationVersionNameLinux=")
+//            // Android平台属性
+//            writer.println("\n## Android")
+//            writer.println("#applicationIdAndroid=")
+//            writer.println("#applicationNameAndroid=")
+//            writer.println("#applicationVersionCodeAndroid=")
+//            writer.println("#applicationVersionNameAndroid=")
+//
+//            // iOS平台属性
+//            writer.println("\n## IOS")
+//            writer.println("#applicationIdIOS=")
+//            writer.println("#applicationNameIOS=")
+//            writer.println("#applicationVersionCodeIOS=")
+//            writer.println("#applicationVersionNameIOS=")
+//
+//            // Web平台属性
+//            writer.println("\n## Web")
+//            writer.println("#applicationIdWeb=")
+//            writer.println("#applicationNameWeb=")
+//            writer.println("#applicationVersionCodeWeb=")
+//            writer.println("#applicationVersionNameWeb=")
+//
+//            // Windows平台属性
+//            writer.println("\n## Windows")
+//            writer.println("#applicationIdWindows=")
+//            writer.println("#applicationNameWindows=")
+//            writer.println("#applicationVersionCodeWindows=")
+//            writer.println("#applicationVersionNameWindows=")
+//
+//            // MacOS平台属性
+//            writer.println("\n## MacOS")
+//            writer.println("#applicationIdMacOs=")
+//            writer.println("#applicationNameMacOs=")
+//            writer.println("#applicationVersionCodeMacOs=")
+//            writer.println("#applicationVersionNameMacOs=")
+//
+//            // Linux平台属性
+//            writer.println("\n## Linux")
+//            writer.println("#applicationIdLinux=")
+//            writer.println("#applicationNameLinux=")
+//            writer.println("#applicationVersionCodeLinux=")
+//            writer.println("#applicationVersionNameLinux=")
 
         }
     }
